f351d30151567898078f3ae28df1dc76e7a5e7fa
smshah121 <smshah.2003@gmail.com>
2026-02-28 15:49:04 +0500

    cs

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
   app.enableCors({
-    origin: "*",
+    origin: ['https://pixora-media.netlify.app'],
   })
   await app.listen(process.env.PORT ?? 3000);
 }
